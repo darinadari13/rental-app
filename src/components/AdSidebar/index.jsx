d8e42d5dec9d469a19af4810e89ff91e0173e57f
Dari <darina460@gmail.com>
2022-08-24 13:46:18 +0300

    chore

--- src/components/AdSidebar/index.jsx
+++ src/components/AdSidebar/index.jsx
@@ -1,17 +1,22 @@
 
 import React, { useEffect, useState } from 'react';
 import classes from './index.module.scss';
-import { List } from 'antd'
+import { List, Button, message,  Popconfirm  } from 'antd'
 import { getFileDowloadUrl } from '../../api'
 
 function AdSidebarItem({ item }) {
   const [imageUrl, setImageUrl] = useState() 
-
   useEffect(() => {
     getFileDowloadUrl(item.imagePath).then(setImageUrl)
   }, [])
+  const confirm = () => {
+  message.info('You deleted your ad');
+};
   return (
     <List.Item className={classes.item}>
+     <Popconfirm placement="left" title={'Are you sure you want to delete ad?'} onConfirm={confirm} okText="Yes" cancelText="No">
+        <Button>x</Button>
+      </Popconfirm>
       {imageUrl && <img src={imageUrl} alt=''/>}
       <span>{item.title}</span>
       <span>{item.price} UAH</span>
@@ -22,6 +27,7 @@ function AdSidebarItem({ item }) {
 function AdSidebar({ adList }) {
   return (
     <div className={classes.root}>
+
       <List
         size="large"
         bordered
